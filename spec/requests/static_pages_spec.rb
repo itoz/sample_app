864813f91bcb52f536d07ec0cfe0f6168ccada8a
itoz <romatica@gmail.com>
2015-03-02 17:23:12 +0900

    static page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -71,15 +71,13 @@ describe "StaticPages" do
             click_link "sample app"
             expect(page).to  have_title(full_title(''))
         end
-
     end
+
+
     # describe "GET /static_pages" do
     #   it "works! (now write some real specs)" do
     # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
     # get static_pages_index_path
     # response.status.should be(200)
-
-
-    # end
     # end
 end
