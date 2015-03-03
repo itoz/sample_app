c8a5af77ea22d240c7d5b8f4640bf7ef2187d554
itoz <romatica@gmail.com>
2015-03-03 17:09:17 +0900

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,30 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-    # @user = User.find_by(id: params[:id])
-    # @user = User.find_by_id(params[:id])
-  end
+    def show
+        @user = User.find(params[:id])
+        # @user = User.find_by(id: params[:id])
+        # @user = User.find_by_id(params[:id])
+    end
 
-  def new
-  end
+    def new
 
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            #保存の成功
+            flash[:success] = "Welcome to the Sample App!"
+            redirect_to @user
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+    #Strong Parameter
+    def user_params
+        params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,6 +4,8 @@ describe "UserPages" do
 
   subject {page}
 
+
+  #プロフィールページの表示
   describe "profile page" do
     let(:user) {FactoryGirl.create(:user)}
     before {visit user_path(user)}
@@ -12,6 +14,7 @@ describe "UserPages" do
 
   end
 
+  #サインアップページの表示
   describe "signup page" do
     before {visit signup_path}
     it {should have_content('Sign up')}
@@ -20,5 +23,42 @@ describe "UserPages" do
 
 
 
+  #サインアップ
+
+  describe "signup" do
+
+
+    before {visit signup_path}
+
+    let(:submit) {"Create my account"}
+
+    #登録の失敗
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect{click_button submit}.not_to change(User,:count)
+      end
+    end
+
+
+    #登録の成功
+
+    describe "with valid information" do
+      before do
+        fill_in "Name", with: "Example User"
+        fill_in "Email", with: "user@example.com"
+        fill_in "Password", with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+      #カウントが1ふえる
+      it "should create a user" do
+        # expect { click_button submit }.to change(User,:count).by(1)
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+
+    end
+
+
+  end
 
 end
